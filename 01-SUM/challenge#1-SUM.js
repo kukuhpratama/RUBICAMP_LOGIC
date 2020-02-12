f11c4e2e7e0d336202737f1c47b752afec289df1
kukuh01 <kukuh.pratama0111@gmail.com>
2020-02-12 16:08:19 +0700

    Add Challenge 01

---
+++ 01-SUM/challenge#1-SUM.js
@@ -0,0 +1,13 @@
+function sum() {
+  total = 0;
+  for (var i = 0; i < arguments.length; i++) {
+    total += arguments[i];
+  }
+  console.log(total);
+
+}
+
+sum(1, 2, 7);
+sum(1, 4);
+sum(11);
+sum(10, 3, 6, 7, 9);
